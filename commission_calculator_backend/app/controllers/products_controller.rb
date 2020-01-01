dd3af83ba63cebe9e0f7f9e9f3b861852951ae6c
judy <jjeanius@verizon.net>
2020-01-01 00:17:49 -0500

    correcting  product codes

--- commission_calculator_backend/app/controllers/products_controller.rb
+++ commission_calculator_backend/app/controllers/products_controller.rb
@@ -5,28 +5,36 @@ class ProductsController < ApplicationController
         render json: @products
     end
 
-    def show
-        @product = Product.find_by(id: params[:id])
-        redner json: @products
-    end
-
     def new
-        @product = Product.create(product_params)
+        @product = Product.new
         render json: @products
     end
 
-    def edit
-        @product = Product.find_by(id: params[:id])
-
-        if @prodruct.save
-            render json: @products
-        else
-            render 'edit'
+    def create
+        @product = Product.new(paroduct_params)
+            if @product.save
+                render json: @products
+            else
+                render 'new'
+            end
         end
+
+    def show
+        set_product
+        redner json: @products
+    end
+
+    def edit
+        set_product
+            if @prodruct.save
+                render json: @products
+            else
+                render 'edit'
+            end
     end
 
     def update
-        @product = Product.find_by(id: params[:id])
+        set_product
         if @product.update
             render json: @products
         else
@@ -35,16 +43,19 @@ class ProductsController < ApplicationController
     end
 
     def destory
-        @product = Product.find_by(id: params[:id])
+        set_product
         @product.delete
-        render json: @Products
+        render json: @products
     end
 
     private
 
-    def product_params
-        params.require(:product).permit(:name)
-    end
+        def product_params
+            params.require(:product).permit(:name)
+        end
 
+        def set_product
+            @product = Product.find_by(id: params[:id])
+        end
 
 end
