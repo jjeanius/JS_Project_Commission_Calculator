13ee28ca13683f1ec144410e7987838efdae297c
judy <jjeanius@verizon.net>
2020-01-25 11:07:55 -0500

    working on frontend

--- commission_calculator_frontend/src/index.js
+++ commission_calculator_frontend/src/index.js
@@ -1,5 +1,54 @@
-//document.addEventListener
-
-const Base_URL = "http://localhost:3000"
+/*const Base_URL = "http://localhost:3000/"
 const Product_URL = '${Base_URL}'/products
 const Sales_URL = '${Sales_URL}'/sales
+const mainDiv = document.querySelector('main') */
+
+let buttonCalculate = document.getElementById('buttonCalculate')
+let buttonReset = document.getElementById('buttonReset')
+
+buttonCalculate.addEventListener('click', calculate)
+    function calculate(ev){
+        console.log(ev.type, ev.target, ev.currentTarget)
+    }
+
+buttonReset.addEventListener('click', reset)
+    function reset(ev){
+        console.log(ev.type, ev.target, ev.currentTarget)
+    }
+
+
+
+
+
+//const productName = document.getElementById(commission-sales-form)
+
+
+       /* return fetch(Product_URL)
+            .then (response => response.json())
+            .then(getProducts)
+
+    function getProducts(products)
+        products.forEach(product =>{
+            let productString = " "
+        product.sales.forEach(sale => {
+            productString +=<li> ${product.name} </li>
+        })
+
+
+
+ document.getElementById('Calculate').addEventListener('click, upload');
+
+
+    function upload(ev){
+        ev.preventDefault();   //stop the form submitting
+    }
+
+
+
+function calculate() {
+    calculate_commission_percentage;
+}
+
+}
+
+*/
