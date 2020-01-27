c745f31443feec656ecaa2936e624642c8ff587b
judy <jjeanius@verizon.net>
2020-01-27 00:06:33 -0500

    working on forms

--- commission_calculator_frontend/src/index.js
+++ commission_calculator_frontend/src/index.js
@@ -5,31 +5,34 @@ const mainDiv = document.querySelector('main') */
 
 let buttonCalculate = document.getElementById('buttonCalculate');
 let buttonReset = document.getElementById('buttonReset');
-
 let inputs = document.querySelectorAll('#commission-sales-form .input');  //nodelist
 
-Array.from(inputs).forEach(function(input){
-    input.addEventListener('input', addInput);
+Array.from(inputs).forEach(function(input){  //change to array and iterate through the inputs nodelist
+    input.addEventListener('input', addInput);  // add event listener to each input
         function addInput(ev){
             console.log(ev.target, ev.target.value);
-
         };
 })
 
-/*let txt = document.getElementById('text');
-let inputPrice = document.getElementById('price');
-let inputQuantity = document.getElementById('quantity');
-let inputCommissionRate = document.getElementById('commission-rate');  */
-
-
-buttonCalculate.addEventListener('click', calculate);
+/* buttonCalculate.addEventListener('click', calculate);  // add event listener to the calculate button
     function calculate(ev){
         console.log(ev.type, ev.target, ev.currentTarget);
-    }
+    }  */
 
-buttonReset.addEventListener('click', reset);
-    function reset(ev){
-        console.log(ev.type, ev.target, ev.currentTarget);
+buttonReset.addEventListener('click', function(ev){  // add even listener to the reset button
+    if(ev.target.classInput == 'reset'){
+       const li = e.target.parentElement;
+       buttonReset.removeChild(li)
     }
+    });
+
+    const addForm = document.forms['commission-sales-form'];   // create commission sales form
+        addForm.addEventListener('submit', function(e){     // add even listener to the button
+            e.preventDefault();
+            const product = addForm.querySelector('input[type="text"]').value;   
+            console.log(product)
+
+        })
+
 
 
