67645c56d7bf950c6ee20a753778b2f7115a69b2
judy <jjeanius@verizon.net>
2020-02-04 12:23:33 -0500

    create a copy commission calculator

--- commission_calculator_frontend/src/components/sale.js
+++ commission_calculator_frontend/src/components/sale.js
@@ -1,5 +1,6 @@
 class Sale{         // create a new instances of sale
     constructor(){  // kick off constructor
+
         this.products = new Products()  // create new instances of our products class
         }
     }
