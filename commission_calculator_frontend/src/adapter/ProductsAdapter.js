b60416d183f82c9bfc610e930fe38e6aadb4a08c
judy <jjeanius@verizon.net>
2020-02-18 23:02:55 -0500

    change from json to text

--- commission_calculator_frontend/src/adapter/ProductsAdapter.js
+++ commission_calculator_frontend/src/adapter/ProductsAdapter.js
@@ -1,14 +1,15 @@
 class ProductsAdapter{  //talk to our backend API
     constructor(){  // object with our constructor
-        this.baseUrl =    // instantiate with the adapter, would set base url
-        'http://localhost:3000/sales'   //  and has ability to call get Product
-    }
+        this.baseUrl = 'http://localhost:3000/products'   // instantiate with the adapter, would set base url
+          }      //  and has ability to call get Product
 
     getProducts(){     // make a fetch request to our base url
         return fetch(this.baseUrl)   // once it recv response, we parce json from the response
-        .then(response =>response.json())
-        }
+        .then(response =>response.text())
+        .then(text => console.log(text))
+      }
     }
+
 
 // adapter = new ProductAdapter()
 // adapter.getProducts() // this will get product from our database
