bdbee2d6c0d6664ca74b4582583353eb11b372ca
judy <jjeanius@verizon.net>
2020-02-28 14:54:50 -0500

    adding EventListener to DOM

--- commission_calculator_frontend/src/components/sales.js
+++ commission_calculator_frontend/src/components/sales.js
@@ -2,25 +2,57 @@ class Sales {         // create a new instances of sale
     constructor(){
        this.sales = []  // set property of sale and set it to an empty array
        this.adapter = new SalesAdapter()  //  create adapter
-       // this.bindEventListerners()  //at somepoint we want to evoke a method eventlisterner
+       this.addEventListeners()  //at somepoint we want to evoke a method eventlisterner
        this.fetchAndLoadSales()  // evoke the method
     }
-        fetchAndLoadSales(){
-          this.adapter
-          .getSales()
-          .then(sales => {
-              sales.forEach(sale => this.sales.push(new Sale(sale)))  // push new sale instance into Sales array and passing in saleJson
-          })
-          .then(() => {
-              this.render()
+
+    addEventListeners(){
+       const calculateButton = document.getElementById('buttonCalculate');
+       calculateButton.addEventListener('click', calculate);
+            function calculate(ev){
+               ev.preventDefault()
+
+
+               console.log(ev.type, ev.target, ev.currentTarget);
+        }
+
+       const resetButton = document.getElementById('buttonReset')
+       resetButton.addEventListener('click', reset);
+            function reset(ev){
+               console.log(ev.type, ev.target, ev.currentTarget);
+            }
+    }
+
+    fetchAndLoadSales(){
+       this.adapter
+       .getSales()
+       .then(sales => {
+           sales.forEach(sale => this.sales.push(new Sale(sale)))  // push new sale instance into Sales array and passing in saleJson
+        })
+        .then(() => {
+        this.render()
           })
          }
 
          render() {
-             const salesContainer = document.getElementById('product_column');
-             salesContainer.innerHTML += this.sales.map(sale => `<li>${sale.sale.product.name}<li>`).join('')
+        //    console.log(this.sales)
+
+            const productContainer = document.getElementById('product_column');
+             productContainer.innerHTML += this.sales.map(sale => `<li>${sale.sale.product.name}<li>`).join(' ')
 
             const priceContainer = document.getElementById('price_column');
-            priceContainer.innerHTML += this.sales.map(sale => `<li> ${sale.sale.price} </li>`).join('')
+            priceContainer.innerHTML += this.sales.map(sale => `<li> ${sale.sale.price} </li>` ).join(' ')
+
+            const quantityContainer = document.getElementById('quantity_column');
+            quantityContainer.innerHTML += this.sales.map(sale => `<li> ${sale.sale.quantity} </li>`).join(' ')
+
+            const rateContainer = document.getElementById('rate_column');
+            rateContainer.innerHTML += this.sales.map(sale => `<li> ${sale.sale.commission_rate} </li>`).join(' ')
+
+            const commissionContainer = document.getElementById('commission_column');
+            commissionContainer.innerHTML += this.sales.map(sale =>`<li> ${sale.sale.commission_amount}</li>`).join(' ')
+
+            const deleteCommission = document.getElementById('delete_commission');
+            deleteCommission.innerHTML += this.sales.map(sale => `<li>Delete</li>`).join(' ')
          }
      }
